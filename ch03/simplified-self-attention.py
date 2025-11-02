732f9b5b1e4796e50825a2a59d9587e33d7707a4
urmi22 <debjanimazumder22@gmail.com>
2025-11-02 18:10:51 +0530

    updated

--- ch03/simplified-self-attention.py
+++ ch03/simplified-self-attention.py
@@ -1,5 +1,11 @@
+# generate context vector for x2
+
 import pdb
 import torch
+
+
+def softmax_naive(x):
+    return torch.exp(x) / torch.exp(x).sum(dim=0)
 
 inputs = torch.tensor(
     [
@@ -23,8 +29,23 @@ print(attention_scores_2)
 # divided by sum normalization
 attention_weights_2_tmp = attention_scores_2 / attention_scores_2.sum()
 print(f"Attention weights: {attention_weights_2_tmp}")
+print(f"Sum: {attention_weights_2_tmp.sum()}")
 
 #use softmax function for normalization --> 1) better at managing extreme values, 2) offers more favorable gradients  propoerties during training
+#                                           3) ensures attention weights betweeb 0 to 1 (always positive)
 
+attention_weights_2_naive = softmax_naive(attention_scores_2)
+print(f"Attention weights: {attention_weights_2_naive}")
+print(f"Sum: {attention_weights_2_naive.sum()}")
+
+# softmax_naive may produce numerical instability for large or smaill imput values, so its better to use PyTorch's in built softmax function
+attention_weights_2 = torch.softmax(attention_scores_2, dim=0)
+print(f"Attention weights: {attention_weights_2}")
+print(f"Sum: {attention_weights_2.sum()}")
+
+context_vector_2 = torch.zeros(query.shape)
+for i, x_i in enumerate(inputs):
+    context_vector_2 += attention_weights_2[i] * x_i
+print(context_vector_2)
 
 pdb.set_trace()
