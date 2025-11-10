ad7b54c2be3ffb7571e3936ce3d135825a002924
urmi22 <debjanimazumder22@gmail.com>
2025-11-10 12:07:33 +0530

    updated

---
+++ ch04/dummy_GPT_model.py
@@ -0,0 +1,47 @@
+# A placeholder GPT model architecture class
+
+import pdb
+import torch
+import torch.nn as nn
+from GPT_CONFIG_124M import GPT_CONFIG_124M as cfg
+
+
+
+class DummyGPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.transformer_blocks = nn.Sequential(*[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias = False)
+
+
+    def forward(self, input_idx):
+        batch_size, seq_len = input_idx.shape
+        tok_embeds = self.tok_emb(input_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=input_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.transformer_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+
+class DummyTransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+    
+
+class DummyLayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps = 1e-5):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+    
