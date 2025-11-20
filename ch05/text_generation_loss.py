e413adcbfb937710e999ee54f89520e0dee541b5
urmi22 <debjanimazumder22@gmail.com>
2025-11-20 12:11:47 +0530

    updated

--- ch05/text_generation_loss.py
+++ ch05/text_generation_loss.py
@@ -38,13 +38,47 @@ def main():
     print(f"Probabilities of Inputs: {probas}")
     print(probas.shape)
     token_ids = torch.argmax(probas, dim=-1, keepdim=True)
-    print(f"Token IDs: {token_ids}")
+    print(f"Output Token IDs: {token_ids}")
 
     target_text = token_ids_to_text(targets[0], tokenizer)
     output_text = token_ids_to_text(token_ids[0].flatten(), tokenizer)
     print(f"Targets batch 0: {target_text}")
     print(f"Output batch 0: {output_text}")
-    pdb.set_trace()
+
+    # we can print the initial softmax probability scores corresponding to the target tokens using the following code
+    text_idx = 0
+    target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+    print(f"Text 1: {target_probas_1}")
+    text_idx = 1
+    target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+    print(f"Text 2: {target_probas_2}")
+
+    # calculate negative average log probabilities
+    log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+    print(f"log probas: {log_probas}")
+    avg_log_probas = torch.mean(log_probas) 
+    print(f"average log prob: {avg_log_probas}")
+    neg_avg_log_probas = avg_log_probas * -1
+    print(f"Negative average log prob: {neg_avg_log_probas}")
+
+    # calculate cross-entropy loss
+    print("Logits shape:", logits.shape)
+    print("Targets shape:", targets.shape)
+
+    '''
+    For the cross_entropy loss function in PyTorch, we want to flatten these tensors by combining them over the batch dimension
+    PyTorch’s cross_entropy function will take care of all these steps for us.
+
+    '''
+    logits_flat = logits.flatten(0, 1)
+    targets_flat = targets.flatten()
+    print("Flattened logits:", logits_flat.shape)
+    print("Flattened targets:", targets_flat.shape)
+    loss = nn.functional.cross_entropy(logits_flat, targets_flat)
+    print(f"Cross entropy loss: {loss}")
+    perplexity = torch.exp(loss)
+    print(f"Perplexity: {perplexity}")
+    
 
 
 
