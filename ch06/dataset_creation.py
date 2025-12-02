7bd8cbba3ea8b29eed5e7e7c201f5c523301a933
urmi22 <debjanimazumder22@gmail.com>
2025-12-03 01:04:29 +0530

    updated

--- ch06/dataset_creation.py
+++ ch06/dataset_creation.py
@@ -1,6 +1,22 @@
 import pdb
 import pandas as pd
 
+def random_split(df, train_frac, validation_frac):
+
+    # Shuffles the entire DataFrame
+    df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+
+    # Calculates split indices
+    train_end = int(len(df) * train_frac)
+    validation_end = train_end + int(len(df) * validation_frac)
+
+    # Splits the DataFrame
+    train_df = df[:train_end]
+    validation_df = df[train_end:validation_end]
+    test_df = df[validation_end:]
+
+    return train_df, validation_df, test_df
+
 
 
 def create_balanced_dataset(df):
@@ -13,6 +29,7 @@ def create_balanced_dataset(df):
 
     # Combines ham subset with “spam”
     balanced_df = pd.concat([ham_subset, df[df["Label"] == "spam"]])
+
     return balanced_df
 
 
@@ -29,6 +46,21 @@ def main():
 
     # we convert the “string” class labels "ham" and "spam" into integer class labels 0 and 1, respectively:
     balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1})
+
+    '''
+    Next, we create a random_split function to split the dataset into three parts: 
+    70% for training, 10% for validation, and 20% for testing.
+
+    '''
+    # Test size is implied to be 0.2 as the remainder.
+    train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)
+
+    # save the dataset as CSV (comma-separated value) files so we can reuse it later
+    train_df.to_csv("./ch06/input_dataset/train.csv", index=None)
+    validation_df.to_csv("./ch06/input_dataset/validation.csv", index=None)
+    test_df.to_csv("./ch06/input_dataset/test.csv", index=None)
+
+    
     
 
 
